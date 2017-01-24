5e09d86a6bded2200b37575a877efe1555aa08aa
Subasish Mohapatra <subasish.mohapatra@hotmail.com>
2017-01-24 14:26:07 +0530

    Added new algo to find list sort order

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,4 +1,5 @@
-﻿using StudentCollegeRegistration;
+﻿using SinglePageAlgos;
+using StudentCollegeRegistration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +11,56 @@ namespace Client
     class Program
     {
         static void Main(string[] args)
+        {
+            #region WAP to view list of registered colleges by a student
+
+            //ViewRegisteredColleges();
+
+            #endregion
+
+            #region WAP to find sorting order of List<date>
+
+            //List<DateTime> having  more than 10 dates.How to find whether the dates are arranged in monthly basis or yearly basis or without any order.
+            ListSortOrder();
+
+            #endregion
+
+            Console.ReadLine();
+        }
+
+        private static void ListSortOrder()
+        {
+            List<DateTime> datesYearSorted = new List<DateTime>();
+            List<DateTime> datesMonthSorted = new List<DateTime>();
+            List<DateTime> datesNotSorted = new List<DateTime>();
+
+            datesYearSorted.Add(new DateTime(2010, 2, 15));
+            datesYearSorted.Add(new DateTime(2011, 1, 15));
+            datesYearSorted.Add(new DateTime(2012, 5, 15));
+            datesYearSorted.Add(new DateTime(2013, 8, 15));
+            datesYearSorted.Add(new DateTime(2014, 12, 15));
+            datesYearSorted.Add(new DateTime(2015, 9, 15));
+
+            datesMonthSorted.Add(new DateTime(2011, 3, 15));
+            datesMonthSorted.Add(new DateTime(2009, 4, 15));
+            datesMonthSorted.Add(new DateTime(2012, 5, 15));
+            datesMonthSorted.Add(new DateTime(2010, 6, 15));
+            datesMonthSorted.Add(new DateTime(2015, 7, 15));
+            datesMonthSorted.Add(new DateTime(2008, 8, 15));
+
+            datesNotSorted.Add(new DateTime(2016, 2, 15));
+            datesNotSorted.Add(new DateTime(2011, 1, 15));
+            datesNotSorted.Add(new DateTime(2009, 5, 15));
+            datesNotSorted.Add(new DateTime(2012, 8, 15));
+            datesNotSorted.Add(new DateTime(2010, 12, 15));
+            datesNotSorted.Add(new DateTime(2015, 9, 15));
+
+            Console.WriteLine($"{DateListSortOrder.CheckDateOrder(datesMonthSorted)}");
+            Console.WriteLine($"{DateListSortOrder.CheckDateOrder(datesNotSorted)}");
+            Console.WriteLine($"{DateListSortOrder.CheckDateOrder(datesYearSorted)}");
+        }
+
+        private static void ViewRegisteredColleges()
         {
             var registar = new CollegeRegistration();
 
@@ -61,8 +112,6 @@ namespace Client
             {
                 Console.WriteLine($"{college.Name}");
             }
-
-            Console.ReadLine();
         }
     }
 }
---
+++ SinglePageAlgos/DateListSortOrder.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinglePageAlgos
+{    
+    //This class checks the sorted order of a list<DateTime> whether 
+    public class DateListSortOrder
+    {  
+        public static string CheckDateOrder(List<DateTime> test)
+        {
+            IEnumerable<DateTime> temp;
+
+            temp=from item in test orderby item.Year select item;
+
+            if (test.SequenceEqual(temp))
+                return "Sorted by year";
+            else
+            {
+                temp = from item in test orderby item.Month select item;
+                if (test.SequenceEqual(temp))
+                    return "Sorted by month";
+            }
+            return "Not sorted";
+        }
+    }
+}
